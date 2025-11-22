eab3c300e54b149d53561dc1fbb0d0deefab8ecb
HOLGUINUPC <u202220250@upc.edu.pe>
2025-11-22 11:55:11 -0500

    feat: added api-gateway

---
+++ src/test/java/com/agrocontrol/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrocontrol.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
