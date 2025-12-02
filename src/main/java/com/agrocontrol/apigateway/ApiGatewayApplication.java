7c15615620cf812519a7e8ab40201b6b7fb87653
EduardTrave <u20211a789@upc.edu.pe>
2025-12-02 04:31:57 -0500

    fix: configuración actualizada del api-gateway

--- src/main/java/com/agrocontrol/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/agrocontrol/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,10 @@ package com.agrocontrol.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +13,20 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:4200");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+
 
 }
